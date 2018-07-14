b893f5f5e497be104895ad757dd7ed4ad1d99713
Michael Dragomir <michaeldragomir@mac.com>
2018-07-14 09:35:52 -0700

    additions

--- src/components/Sandwich/Sandwich.js
+++ src/components/Sandwich/Sandwich.js
@@ -9,23 +9,31 @@ import SandwichIngredient from './SandwichIngredient/SandwichIngredient';
 // we add a div to this sandwich wrapper to give it some styleing for the sandwich
 // there has to be a type since we're using PropTypes.
 const sandwich = (props) => {
+  // we need to get the ingredients object and pass it to the sandwich. we need to transform it into an array of the values of the ingredients in the sandwichbuilder.
+  // OBJECT IS A DEFAULT JS OBJECT THAT HAS A KEYS METHOD. THIS EXTRACTS THE KEYS OF A GIVEN OBJECT AND TURNS THAT INTO AN ARRAY. SO IT GIVES YOU AN ARRAY OF THE KEYS.
+
+  // we transfored an object of key/value pairs into an array of sandwich ingredients  where the value of the object helps decide how many ingredients is needed and the key to decide which type of ingredient.
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <SandwichIngredient key={ingKey + i} type={ingKey} />;
+      });
+    })
+    // we use reduce to pull out the values from the array and create a new array with those values. reduce takes two arguments. the previous value and the current value.  we are add the el to the current arr.
+    //  we flatened the array.. with the reduce method.
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+    // if its emtpy we can check for it.
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <div> Customize your delicious Sandwich!</div>;
+  }
+  console.log(transformedIngredients);
+
   return (
     <div className={classes.Sandwich}>
       <SandwichIngredient type="bread-top" />
-      <SandwichIngredient type="cheese" />
-      <SandwichIngredient type="ham" />
-      <SandwichIngredient type="cheese" />
-      <SandwichIngredient type="ham" />
-      <SandwichIngredient type="salad" />
-      <SandwichIngredient type="cheese" />
-      <SandwichIngredient type="ham" />
-      <SandwichIngredient type="cheese" />
-      <SandwichIngredient type="ham" />
-      <SandwichIngredient type="tomato" />
-      <SandwichIngredient type="cheese" />
-      <SandwichIngredient type="ham" />
-      <SandwichIngredient type="cheese" />
-      <SandwichIngredient type="ham" />
+      {transformedIngredients}
       <SandwichIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/SandwichBuilder/SandwichBuilder.js
+++ src/containers/SandwichBuilder/SandwichBuilder.js
@@ -3,10 +3,20 @@ import Aux from '../../hoc/Aux';
 import Sandwich from '../../components/Sandwich/Sandwich';
 
 class SandwichBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 0,
+      ham: 0,
+      cheese: 0,
+      tomato: 0
+    }
+  }
+
   render () {
     return (
       <Aux>
-        <Sandwich />
+        <Sandwich ingredients={this.state.ingredients}/>
         <div>Sandwich controls</div>
       </Aux>
     );
