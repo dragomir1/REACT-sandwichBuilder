8cf346fed93480e009be330b67c0a3ee0282354f
Michael Dragomir <michaeldragomir@mac.com>
2018-08-23 14:35:29 -0700

    additions

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -30,6 +30,9 @@ export const authFail = (error) => {
 };
 
 export const authLogOut = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT,
   };
@@ -58,6 +61,8 @@ export const checkAuthTimeout = (expirationTime) => {
 };
 
 
+
+
 // this is the actionCreator that holds the asynch code.
 export const auth = (email, password, isSignUp) => {
   return dispatch => {
@@ -75,6 +80,11 @@ export const auth = (email, password, isSignUp) => {
     axios.post(url, authData)
       .then(response => {
         console.log(response);
+        localStorage.setItem('token', response.data.idToken);
+        // new Date(new Date().getTime() + response.data.expiresIn * 1000); this code turns the date back into an object
+        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.locaId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
 
@@ -83,5 +93,22 @@ export const auth = (email, password, isSignUp) => {
         console.log(err);
         dispatch(authFail(err.response.data.error));
       });
+  };
+};
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if(!token) {
+      dispatch(authLogOut());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if(expirationDate < new Date()) {
+        dispatch(authLogOut());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()) / 1000);
+      }
+    }
   };
 };
